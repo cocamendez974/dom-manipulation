42f3deca6f78ce9a3fbeacb614ebff94fc27be4a
Oscar Adolfo Méndez Muñoz <cocamendez974@gmail.com>
2023-06-22 00:04:28 -0600

    primer commit

---
+++ index.js
@@ -0,0 +1,15 @@
+// ir a gitignore.io
+//crear gitignore para mac, windows, linux, vsc, node.
+//la carpeta node modules es gigante y no se debe agregar.
+//no hacer git add -A porque eso agregaría todos los archivo
+//primero agregar el gitignore con 
+// git add .gitignore
+// git status
+// en .gitignore agregar la linea
+//  .vscode
+// despues de ### VisualStudioCode ###
+// git add .gitignore
+// git add -A
+// git branch -M main
+// git remote add origin https://github.com/cocamendez974/dom-manipulation.git
+// git push --set-upstream origin main   SETEA MAIN COMO LA RAMA PREDETERMINDADA A LA QUE SE LE HACE PUSH
